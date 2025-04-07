cbc4345c06e832a2fe14aac4fb89f713570f3036
Thejesh <thejesh245@gmail.com>
2025-04-07 13:07:23 +0530

    Update tests.py

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class MyViewTestCase(TestCase):
+    def test_home_view(self):
+        """Test if website is available or not-  returns 200 status"""
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
